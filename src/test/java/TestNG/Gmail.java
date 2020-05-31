3210b84b9143e122fb4234b009acf6ed4e39339d
ganesh <ganesh.kmahalingam@gmail.com>
2020-05-31 18:58:58 +0530

    Extent Report withListener

--- src/test/java/TestNG/Gmail.java
+++ src/test/java/TestNG/Gmail.java
@@ -6,50 +6,17 @@ import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
-import org.testng.ITestResult;
-import org.testng.annotations.AfterMethod;
+import org.testng.SkipException;
 import org.testng.annotations.AfterSuite;
-import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeSuite;
-import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
-
-import com.aventstack.extentreports.ExtentReports;
-import com.aventstack.extentreports.ExtentTest;
-import com.aventstack.extentreports.markuputils.ExtentColor;
-import com.aventstack.extentreports.markuputils.Markup;
-import com.aventstack.extentreports.markuputils.MarkupHelper;
-import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
-import com.aventstack.extentreports.reporter.configuration.Theme;
 
 public class Gmail {
 	
 	
-	public ExtentHtmlReporter htmlReporter;
-	public ExtentReports extent;
-	public ExtentTest test;
 	
 	
-	
-	
-	@BeforeTest
-	public void setReport(){
-		htmlReporter=new ExtentHtmlReporter("./reports/extent.html");
-		htmlReporter.config().setEncoding("utf-8");
-		htmlReporter.config().setDocumentTitle("Test Result");
-		htmlReporter.config().setTheme(Theme.STANDARD);
-		
-		extent=new ExtentReports();
-		extent.attachReporter(htmlReporter);
-		
-		extent.setSystemInfo("Automation Report", "Ganesh");
-	}
-
-	@BeforeClass
-	public void endReport(){
-		extent.flush();	
-	}
 	
 	WebDriver driver;
 
@@ -73,7 +40,7 @@ public class Gmail {
 	@Parameters("browser")
 	public void launchBrowser(String bN){
 		
-		test=extent.createTest("Launch Browser");
+		
 		if(bN.equalsIgnoreCase("chrome")){
 		System.out.println("Launching Chrome Browser -->"+bN);	
 		}
@@ -82,50 +49,39 @@ public class Gmail {
 
 	@Test(priority=1)
 	public void loadGmail(){
-		test=extent.createTest("Loading Gmail");
+		
 		System.out.println("Loading Gmail");
 		Assert.assertEquals("gmail", "gmail");
 	}
 
 	@Test(priority=2,dependsOnMethods="loadGmail")
 	public void composeMail(){
-		test=extent.createTest("Compose Gmail");
 		System.out.println("Composing G-Mail");		
 	}
 	
 	@Test(priority=3,dependsOnMethods="loadGmail")
 	public void Logout(){
-		test=extent.createTest("Logout Gmail");
 		System.out.println("Gmail Logout");		
 	}
 
 	@Test(priority=4)
 	public void closeDB(){
-		test=extent.createTest("Closing DB Connection");
+	
 		System.out.println("Closing DB Connection");
 		String expected="E";
-		String actual="E";
+		String actual="A";
 		Assert.assertEquals(expected, actual);
 		
 	}
 	
-	@AfterMethod
-	public void tearDown(ITestResult result){
+	@Test(priority=5)
+	public void skipTestcase(){
+	
+		System.out.println("Test case skip");
+		throw new SkipException("Skiping the Test Case");
 		
-		if(result.getStatus()==ITestResult.SUCCESS){
-			
-			String methodName=result.getMethod().getMethodName();
-			
-			Markup m=MarkupHelper.createLabel(methodName, ExtentColor.GREEN);
-			test.pass(m);
-		}
-		else if(result.getStatus()==ITestResult.FAILURE){
-			
-			String methodName=result.getMethod().getMethodName();
-			
-			Markup m=MarkupHelper.createLabel(methodName, ExtentColor.RED);
-			test.fail(m);
-		}
 	}
-
+	
+	
+	
 }
